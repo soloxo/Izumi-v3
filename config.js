d2b627f8a3a4b43fafe37c8d9f430f1e6e66e3b7
soloxo <117898872+soloxo@users.noreply.github.com>
2024-07-09 17:54:27 +0300

    Update config.js

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ module.exports = {
   ERROR_MSG: toBool(process.env.ERROR_MSG) || true,
   LOG_MSG: toBool(process.env.LOG_MSG) || true,
   READ_CMD: toBool(process.env.READ_CMD),
-  SESSION_ID: process.env.SESSION_ID || "izumi~JTA19aCJ",
+  SESSION_ID: process.env.SESSION_ID || "",
   MENU_URL: process.env.MENU_URL || null,
   CAPTION: process.env.CAPTION || "Iᴢᴜᴍɪ",
   READ_MSG: toBool(process.env.READ_MSG),
